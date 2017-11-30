486e422f152d2d588fd76c9e129f8868269ea5e2
think <leobluewing@qq.com>
2017-11-30 16:40:37 +0800

    清理代码

--- jpaSample/src/main/java/com/auxgroup/jpaSample/chapter1/domain/User.java
+++ jpaSample/src/main/java/com/auxgroup/jpaSample/chapter1/domain/User.java
@@ -1,16 +1,7 @@
 package com.auxgroup.jpaSample.chapter1.domain;
 
-import java.io.Serializable;
-
-import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
-
-import org.hibernate.annotations.GenericGenerator;
 
 @Entity
 @Table(name="user")
